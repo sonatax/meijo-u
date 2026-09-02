c4fda0262b4937d766f602f1285d131dc6951a9a
Yuichi Nakamura <sonata@sfc.wide.ad.jp>
2026-09-02 13:36:11 +0900

    add calc layout

---
+++ app/src/main/java/jp/ac/meijou/android/yuuicnak/MainActivity3.java
@@ -0,0 +1,33 @@
+package jp.ac.meijou.android.yuuicnak;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import jp.ac.meijou.android.yuuicnak.databinding.ActivityMain3Binding;
+
+public class MainActivity3 extends AppCompatActivity {
+
+    private ActivityMain3Binding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        binding = ActivityMain3Binding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        binding.button0.setOnClickListener(view -> {
+
+        });
+    }
+}
